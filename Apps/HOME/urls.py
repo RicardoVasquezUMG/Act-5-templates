fa07b7d226d132aa74f6bb12c42b5cc17cfecc01
T460-CAVASQUEZ\RicardoV <kostasmanolas43@gmail.com>
2025-10-09 19:07:58 -0600

    usuario rejistro

--- Apps/HOME/urls.py
+++ Apps/HOME/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from Apps.HOME import views
-from .views import HomeView
+from .views import HomeView, RegistroView
 
 app_name='home'
 urlpatterns = [
-    path('',HomeView.as_view(), name='homeapp')
+    path('',HomeView.as_view(), name='homeapp'),
+    path('registro/', RegistroView.as_view(), name='registro'),
 ]
--- Apps/HOME/views.py
+++ Apps/HOME/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from Apps.HOME.forms import RegistroForm
+
+from Apps.HOME.models import Usuario
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class RegistroView(CreateView):
+    template_name = 'registro.html'
+    model = Usuario
+    form_class = RegistroForm
+    success_url = reverse_lazy('home:homeapp')
